3403a553098c25d6619c93e416b76b1da5d470fb
Rob Goodall <rsgoodall@gmx.com>
2024-04-27 18:03:12 +0000

    NetBSD 10 (EFI) support added

--- build.sh
+++ build.sh
@@ -58,12 +58,12 @@ EFIMNT=/efi            # efi partition mount point
 export PATH
 file="final.raw"
 mkdir -p ${MNT}
-dd if=/dev/zero of=${file} bs=4096 count=2700000 progress=62000
+dd if=/dev/zero of=${file} bs=4096 count=450000 progress=62000
 vnconfig ${VND} ${file}
 gpt create ${VND}
 [ ${EFI} -eq 1 ] && gpt add -a 2m -l "EFI system" -t efi -s 128m ${VND}
 gpt add -a 4k -l swap -s 512m -t swap ${VND}
-gpt add -a 4k -s 9500m -l root -t ffs ${VND}
+gpt add -a 4k -s 1000m -l root -t ffs ${VND}
 gpt show ${VND}
 
 dkctl ${VND} makewedges
@@ -90,7 +90,7 @@ fi
 
 # newfs dos and ffs
 sleep 2
-newfs -O 2 -n 5000000 -b 4096 /dev/r${dk_dev}
+newfs -O 2 -n 500000 -b 4096 /dev/r${dk_dev}
 mount /dev/${dk_dev} ${MNT}
 
 if echo $version|egrep "^[78]"; then
@@ -144,29 +144,23 @@ echo "PKG_PATH=ftp://ftp.NetBSD.org/pub/pkgsrc/packages/NetBSD/amd64/$version/Al
 #curl -L -k https://github.com/${repo}/archive/${ref}.tar.gz | tar xfz - -C $MNT/tmp
 
 
-#chroot $MNT sh -c '. /etc/profile; pkg_add python310'
-#chroot $MNT sh -c '. /etc/profile; pkg_add 
-#chroot $MNT sh -c '. /etc/profile; cd /tmp/cloud-init-*; PYTHON=/usr/pkg/bin/python3.8 ./tools/build-on-netbsd'
 # Ensure we've got all the dependency installed as expected
-#chroot $MNT sh -c '. /etc/profile; /usr/pkg/bin/python3.8 -mcloudinit.handlers.cloud_config'
 chroot $MNT sh -c '. /etc/profile; pkg_add pkgin'
-chroot $MNT sh -c '. /etc/profile; pkg_add python311'
-chroot $MNT sh -c '. /etc/profile; pkg_add py311-pip'
+chroot $MNT sh -c '. /etc/profile; pkg_add python312'
+chroot $MNT sh -c '. /etc/profile; pkg_add py312-pip'
+chroot $MNT sh -c '. /etc/profile; pkg_add mozilla-rootcerts'
+chroot $MNT sh -c '. /etc/profile; mozilla-rootcerts install'
 chroot $MNT sh -c '. /etc/profile; pkg_add ca-certificates'
-
 echo 'http://ftp.netbsd.org/pub/pkgsrc/packages/NetBSD/$arch/$osrelease/All' > $MNT/usr/pkg/etc/pkgin/repositories.conf
 chroot $MNT sh -c '. /etc/profile; pkgin update'
 chroot $MNT sh -c ". /etc/profile; cat /usr/pkg/etc/ca-certificates-dir.conf | sed 's/#ETCCERTSDIR/ETCCERTSDIR/' > /tmp/ca-certificates-dir.conf; mv /tmp/ca-certificates-dir.conf /usr/pkg/etc/ca-certificates-dir.conf"
 chroot $MNT sh -c '. /etc/profile; update-ca-certificates'
-chroot $MNT sh -c '. /etc/profile; pip3.11 install jsonpointer'
+chroot $MNT sh -c '. /etc/profile; pip3.12 install jsonpointer jsonpatch'
 chroot $MNT sh -c ". /etc/profile; cat /etc/ssh/sshd_config | sed 's/#PasswordAuthentication\ yes/PasswordAuthentication no/' > /tmp/sshd_config; mv /tmp/sshd_config /etc/ssh/sshd_config" 
 
 curl -L -k https://github.com/${repo}/archive/${ref}.tar.gz | tar xfz - -C $MNT/tmp
-chroot $MNT sh -c '. /etc/profile; cd /tmp/cloud-init-*; PYTHON=/usr/pkg/bin/python3.11 ./tools/build-on-netbsd'
-chroot $MNT sh -c '. /etc/profile; /usr/pkg/bin/python3.11 -mcloudinit.handlers.cloud_config'
-echo "datasource_list: ['Exoscale']" >> $MNT/etc/cloud/cloud.cfg
-cp /root/cli/bin/exo $MNT/usr/pkg/bin/
-
+chroot $MNT sh -c '. /etc/profile; cd /tmp/cloud-init-*; PYTHON=/usr/pkg/bin/python3.12 ./tools/build-on-netbsd'
+chroot $MNT sh -c '. /etc/profile; /usr/pkg/bin/python3.12 -mcloudinit.handlers.cloud_config'
 # Disable root account
 test -z "$debug" && chroot $MNT sh -c 'usermod -C yes root'
 chmod +t ${MNT}/tmp
